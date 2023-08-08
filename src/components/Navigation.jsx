15dccbdbbd595b48d6cb00da32b847c5c817487c
Nikhil <malusarend51@gmail.com>
2023-08-09 02:50:39 +0530

    Complete Home Page

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,18 @@
+const Navigation = () => {
+    return <nav>
+    <div>
+      <img src="./images/brand_logo.png" alt="Brand Logo" />
+    </div>
+    <ul>
+      <li href = "">MENU</li>
+      <li href = "">LOCATION</li>
+      <li href = "">ABOUT</li>
+      <li href = "">CONTACT</li>
+    </ul>
+    <button>
+      Login
+    </button>
+  </nav>;
+}
+
+export default Navigation;
